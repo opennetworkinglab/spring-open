b8346d726a9a7d364c8153ac149976df0057a5ed
Yuta HIGUCHI <y-higuchi@onlab.us>
2014-05-12 20:56:54 -0700

    Stop creating RAMCloud jar
    Stop creating RAMCloud jar we no longer use.
    
    Change-Id: I90896e8b9e35d78c1432eb8200d33daabdbd4da9

--- build-ramcloud-java-bindings.sh
+++ build-ramcloud-java-bindings.sh
@@ -8,6 +8,3 @@ export ONOS_HOME=${ONOS_HOME:-$(cd `dirname $0`; pwd)}
 export RAMCLOUD_HOME=${RAMCLOUD_HOME:-~/ramcloud}
 
 ${ONOS_HOME}/ramcloud-build-scripts/build_jni_so.sh
-
-cd ${ONOS_HOME}/target/classes
-jar cfe ${ONOS_HOME}/lib/RamCloud.jar edu.stanford.ramcloud.JRamCloud edu/stanford/ramcloud/*.class
