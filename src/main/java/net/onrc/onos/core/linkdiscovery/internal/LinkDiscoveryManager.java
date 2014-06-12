opennetworkinglab/spring-open

commit 121a024336d37fb2384704f32a00baf6bb174eb7

Link discovery bug fixes.
* Don't send link events if you're not master for the destination switch
* Also, inter-instance links are now discovered unidirectionally (one by
  each instance) rather than bidirectionally by one of the instances.
  The first fix coupled with the strange inter-instance link discovery
  semantics meant that one of the links was dropped.

Change-Id: Ie94090f6ca41863959ecf8d2e8ae194c420bdf17

src/main/java/net/onrc/onos/core/linkdiscovery/internal/LinkDiscoveryManager.java
@@ -861,7 +861,11 @@ public class LinkDiscoveryManager
                     log.trace("Getting standard LLDP from a different controller and quelching it.");
                 }
                 return Command.STOP;
-            } else if (sw <= remoteSwitch.getId()) {
+            }
+            // XXX ONOS: Don't disregard any BDDP messages from other
+            // controllers because they're used for inter-instance link detection
+
+            /*else if (sw <= remoteSwitch.getId()) {
                 if (log.isTraceEnabled()) {
                     log.trace("Getting BBDP from a different controller. myId {}: remoteId {}", myId, otherId);
                     log.trace("and my controller id is smaller than the other, so quelching it. myPort {}: rPort {}", pi.getInPort(), remotePort);
@@ -869,7 +873,7 @@ public class LinkDiscoveryManager
                 //XXX ONOS: Fix the BDDP broadcast issue
                 //return Command.CONTINUE;
                 return Command.STOP;
-            }
+            }*/
             /*
             else if (myId < otherId)  {
                 if (log.isTraceEnabled()) {
@@ -958,8 +962,12 @@ public class LinkDiscoveryManager
             }
         }
 
-        // If the received packet is a BDDP packet, then create a reverse BDDP
-        // link as well.
+        // XXX ONOS: Don't do this:
+        //   If the received packet is a BDDP packet, then create a reverse BDDP
+        //   link as well.
+        // We want to preserve our semantic of the instance that controls the
+        // destination switch is the one who adds the link to the database.
+        /*
         if (!isStandard) {
             Link reverseLink = new Link(lt.getDst(), lt.getDstPort(),
                     lt.getSrc(), lt.getSrcPort());
@@ -971,6 +979,7 @@ public class LinkDiscoveryManager
 
             addOrUpdateLink(reverseLink, reverseInfo);
         }
+        */
 
         // Remove the node ports from the quarantine and maintenance queues.
         NodePortTuple nptSrc = new NodePortTuple(lt.getSrc(), lt.getSrcPort());

src/main/java/net/onrc/onos/core/topology/TopologyPublisher.java
@@ -131,6 +131,12 @@ public class TopologyPublisher implements /*IOFSwitchListener,*/
 
     @Override
     public void linkDiscoveryUpdate(LDUpdate update) {
+        if (!registryService.hasControl(update.getDst())) {
+            // Don't process or send a link event if we're not master for the
+            // destination switch
+            return;
+        }
+
         LinkEvent linkEvent = new LinkEvent(update.getSrc(),
                 (long) update.getSrcPort(), update.getDst(),
                 (long) update.getDstPort());

commit 5337fb8fbe84f9f9e38d0e1c9e94bc6b6c1252cc

Fix for link flapping problem caused by commit 121a0243.
Ports were removed from the maintenance queue which sometimes caused BDDP
packets not to be sent. This meant links timed out and we observed link
flapping.

Change-Id: I2cb27f787bb5424002a426edf74348b9d01f0453

src/main/java/net/onrc/onos/core/linkdiscovery/internal/LinkDiscoveryManager.java
@@ -989,9 +989,13 @@ public class LinkDiscoveryManager
         NodePortTuple nptSrc = new NodePortTuple(lt.getSrc(), lt.getSrcPort());
         NodePortTuple nptDst = new NodePortTuple(lt.getDst(), lt.getDstPort());
         removeFromQuarantineQueue(nptSrc);
-        removeFromMaintenanceQueue(nptSrc);
         removeFromQuarantineQueue(nptDst);
-        removeFromMaintenanceQueue(nptDst);
+
+        // XXX ONOS: Don't remove the port from the maintenance queue here
+        // because it sometimes prevents BDDPs from being sent and causes
+        // links to flap
+        // removeFromMaintenanceQueue(nptSrc);
+        // removeFromMaintenanceQueue(nptDst);
 
         // Consume this message
         return Command.STOP;