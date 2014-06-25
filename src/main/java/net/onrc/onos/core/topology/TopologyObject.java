c45e0b6144974719616c32c22014b5742ee64add
Yuta HIGUCHI <y-higuchi@onlab.us>
2014-06-24 22:34:20 -0700

    Add javadoc
    - Changed constructor to protected, since
      TopologyObject itself is not intended to be instantiated.
    
    Change-Id: I1b407f98504009c04d281ac11380509ec09f3607

--- src/main/java/net/onrc/onos/core/topology/TopologyObject.java
+++ src/main/java/net/onrc/onos/core/topology/TopologyObject.java
@@ -1,11 +1,22 @@
 package net.onrc.onos.core.topology;
 
 
+/**
+ * Base class for Topology Objects.
+ */
 public class TopologyObject {
 
+    /**
+     * Topology instance this object belongs to.
+     */
     protected final Topology topology;
 
-    public TopologyObject(Topology topology) {
+    /**
+     * Constructor.
+     *
+     * @param topology Topology instance this object belongs to
+     */
+    protected TopologyObject(Topology topology) {
         this.topology = topology;
     }
 
