07ace759dfec83fce0182ee8d85ec59d806123f7
Sho SHIMIZU <shimizu.sho@jp.fujitsu.com>
2014-08-21 09:04:41 -0700

    Register intent related types to Kryo serialization
    - Additionally, define a new type for result of intent compilation
    
    Change-Id: I1ba4b143625138273306576a276d62b6344dce4f

---
+++ src/main/java/net/onrc/onos/core/newintent/IntentCompilationResult.java
@@ -0,0 +1,47 @@
+package net.onrc.onos.core.newintent;
+
+import com.google.common.collect.ImmutableList;
+import net.onrc.onos.api.newintent.InstallableIntent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * A class representing a result of intent compilation.
+ */
+public class IntentCompilationResult {
+    private final List<InstallableIntent> result;
+
+    /**
+     * Constructs an instance containing the specified list of installable intents.
+     *
+     * @param result installable intents
+     */
+    public IntentCompilationResult(List<InstallableIntent> result) {
+        checkNotNull(result);
+        checkArgument(result.size() > 0,
+                "result of intent compilation should be " +
+                "more than 1 installable intents or more, but %s", result.size());
+
+        this.result = new ArrayList<>(checkNotNull(result));
+    }
+
+    /**
+     * Constructor for serializer.
+     */
+    protected IntentCompilationResult() {
+        this.result = null;
+    }
+
+    /**
+     * Returns list of installable intents that this object contains.
+     *
+     * @return installable intents
+     */
+    public List<InstallableIntent> getResult() {
+        return ImmutableList.copyOf(result);
+    }
+}
