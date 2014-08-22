b86338ef49919a06e204cb070248d52e98582ee7
Toshio Koide <t-koide@onlab.us>
2014-08-21 19:29:43 -0700

    Implement FlowBatchId class.
    Change-Id: Ic2731d8e725bf89288c38853b8b5f53345afbcd4

--- src/main/java/net/onrc/onos/api/flowmanager/FlowBatchId.java
+++ src/main/java/net/onrc/onos/api/flowmanager/FlowBatchId.java
@@ -1,8 +1,32 @@
 package net.onrc.onos.api.flowmanager;
 
+import java.util.Objects;
+
 /**
  * Represents ID for {@link FlowBatchOperation}.
  */
 public class FlowBatchId {
-    // TODO implement it
+    private final long id;
+
+    /**
+     * Creates a new FlowBatchId object using long value.
+     */
+    public FlowBatchId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return Long.toString(id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return (obj instanceof FlowBatchId) ? id == ((FlowBatchId) obj).id : false;
+    }
 }
